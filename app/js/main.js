d490f13c525c211ce4f42ec0c3025d7a44583e61
Ruslan <camarederic@gmail.com>
2024-10-20 22:10:08 +0300

    added the slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function () {
-  
+  $(".slider__inner").slick({
+    nextArrow:
+      '<button type="button" class="slick-btn slick-next"><img src="icons/angle-right.svg"></button>',
+    prevArrow:
+      '<button type="button" class="slick-btn slick-prev"><img src="icons/angle-left.svg"></button>',
+    infinite: false,
+  });
 });
